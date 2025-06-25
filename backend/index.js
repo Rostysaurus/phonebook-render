da1550881d1741991c11ba2e72fb416e80ad014e
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2025-06-25 16:23:10 +0100

    Add solution for 3.15 - 3.18 to exercises of part3, phonebook

--- backend/index.js
+++ backend/index.js
@@ -2,37 +2,68 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
+
 const app = express()
-
-
-app.use(express.static('dist'))
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body.content) : '')
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+const getPersonsCount = (persons) => persons.length === 1 ? `${persons.length} person` : `${persons.length} people`
+
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.get('/info', (request, response) => {
+  Person.find({})
+    .then(people => {
+      const now = new Date();
+      return response.send(`<h3>Phonebook has info for ${getPersonsCount(people)}</h3><h3>${now}</h3>`)
+    })
+})
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(notes => {
-    response.json(notes)
-  })
+  Person.find({})
+    .then(people => {
+      response.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
- Person.deleteOne({_id: request.params.id}).then(person => {
-   response.json(person)
- })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   if (!request.body) {
-    return response.status(400).json({ error: 'name or number missing' })
+    return response.status(400).json({error: 'name or number missing'})
   }
 
   const {name, number} = request.body
@@ -41,11 +72,39 @@ app.post('/api/persons', (request, response) => {
     name,
     number,
   })
+  person.save()
+    .then(newPerson => {
+      response.json(newPerson)
+    })
+    .catch(error => next(error))
 
-  person.save().then(newPerson => {
-    response.json(newPerson)
-  })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  if (!request.body) {
+    return response.status(400).json({error: 'name or number missing'})
+  }
+  const {name, number} = request.body
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save()
+        .then(newPerson => {
+          response.json(newPerson)
+        })
+    })
+    .catch(error => next(error))
+})
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
